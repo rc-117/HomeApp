619f44eff7444434ada09251c0a5bc5d9c90f870
Richard <wouldcouldyou@gmail.com>
2021-12-11 12:50:03 -0800

    Basic authentication working

--- Homeapp.Backend/Managers/AuthenticationManager.cs
+++ Homeapp.Backend/Managers/AuthenticationManager.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using DummyRepo;
+using DummyRepo.Entities;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
+using System.Linq;
 using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
@@ -29,9 +33,28 @@ namespace Homeapp.Backend.Managers
 
             var authenticationHeaderValue = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
             var bytes = Convert.FromBase64String(authenticationHeaderValue.Parameter);
-            string credentials = Encoding.UTF8.GetString(bytes);
+            string[] credentials = Encoding.UTF8.GetString(bytes).Split(":");
+            string email = credentials[0];
+            string passwordHash = credentials[1];
 
-            return Task.FromResult(AuthenticateResult.Fail("Authorization not implemented"));
+            //Test user from dummy repo
+            User user = Repo.Users.Where(u => 
+                u.EmailAddress == email && u.PasswordHash == passwordHash)
+                .FirstOrDefault();
+
+            if (user == null)
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Invalid username or password."));
+            }
+            else
+            {
+                var claims = new[] { new Claim(ClaimTypes.Name, user.EmailAddress) };
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+                return Task.FromResult(AuthenticateResult.Success(ticket));
+            }
         }
     }
 }
